c1fb535b51dc3b96d3d39be4cd6ba669609f6e4e
Six <23470032+6ixfalls@users.noreply.github.com>
2024-12-12 12:16:52 -0800

    Update entrypoint.sh

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
 mkdir -p /var/lib/postgresql/data/pgdata
-chown postgres:postgres /var/lib/postgresql/data/pgdata
+chown postgres:postgres /var/lib/postgresql/data /var/lib/postgresql/data/pgdata
 su -c "postgres -c config_file=/etc/postgresql/postgresql.conf -c log_min_messages=fatal" postgres
