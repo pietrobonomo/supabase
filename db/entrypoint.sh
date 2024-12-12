e8866cafa20d00fd0e069063da4ddf44c5026863
Six <23470032+6ixfalls@users.noreply.github.com>
2024-12-11 21:03:26 -0800

    fix: get user the first time

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -eo pipefail
-sudo chown postgres:postgres /var/lib/postgresql/data
-chmod 700 /var/lib/postgresql/data
 postgres -c config_file=/etc/postgresql/postgresql.conf -c log_min_messages=fatal
