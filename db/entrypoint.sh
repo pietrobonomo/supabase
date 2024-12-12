12a5cd32cea75944bcf9feaca4ca39c6b4f319e5
Six <23470032+6ixfalls@users.noreply.github.com>
2024-12-11 20:59:30 -0800

    fix: sudo chown

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eo pipefail
-chown --recursive postgres:postgres /var/lib/postgresql/data
+sudo chown --recursive postgres:postgres /var/lib/postgresql/data
 postgres -c config_file=/etc/postgresql/postgresql.conf -c log_min_messages=fatal
